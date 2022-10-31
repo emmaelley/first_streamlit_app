a40ae1755e3da0379b57c1887dbfa09805c0b7b0
emmaelley <80758156+emmaelley@users.noreply.github.com>
2022-10-31 16:14:13 +0000

    streamlit section moved to try except block

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,17 +18,19 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 my_fruit_list = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(my_fruit_list)
 
-# User input to send to API
-streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entered', fruit_choice)
-                                    
 # New Section to display fruityvice api response
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+streamlit.header('Fruityvice Fruit Advice!')
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 
-# Turn json response into a nice pandas dataframe 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 # don't run anything past here while we troubleshoot
 streamlit.stop()
